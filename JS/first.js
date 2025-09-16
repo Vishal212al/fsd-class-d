fb7dd52693d4a9626496887a4ae1e84ec75ddafc
Vishal <vr7533576@gmail.com>
2025-09-16 09:11:44 +0530

    js

---
+++ JS/first.js
@@ -0,0 +1,11 @@
+console.log("Hello, World!");
+document.getElementById("message").innerText= "VISHAL";
+const price1 = 100;
+const prise2 = 15;
+let total = price1 + prise2;
+console.log( total);
+let x = 5;
+let y = 10;
+const total1 = x + y;
+console.log(total1);
+
